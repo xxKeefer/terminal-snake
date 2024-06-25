a9da95b96299f9900aa5405ba6040b83c18a24eb
xxKeefer <xxkeefer.code@gmail.com>
2024-06-25 22:22:06 +1000

    feat: :sparkles: add wall colisions

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import TK, { Terminal, ScreenBuffer } from 'terminal-kit'
 
 type GameState = {
     snake: Snake
+    game: Game
 }
 
 type Snake = {
     position: Coords
+}
+
+type Game = {
+    playing: boolean
+    max: Coords
 }
 
 type Coords = {
@@ -20,14 +26,25 @@ const INITIAL_STATE: GameState = {
             y: 0,
         },
     },
+    game: {
+        playing: true,
+        max: {
+            x: 0,
+            y: 0,
+        },
+    },
 }
 
 const DIRECTIONS = Object.freeze({
-    UP: ['W', 'w', 'UP'],
-    DOWN: ['S', 's', 'DOWN'],
-    LEFT: ['A', 'a', 'LEFT'],
-    RIGHT: ['D', 'd', 'RIGHT'],
+    UP: ['W', 'w', 'UP'] as const,
+    DOWN: ['S', 's', 'DOWN'] as const,
+    LEFT: ['A', 'a', 'LEFT'] as const,
+    RIGHT: ['D', 'd', 'RIGHT'] as const,
 })
+
+type DirectionalInput = (typeof DIRECTIONS)[keyof typeof DIRECTIONS][number]
+type CommandInput = 'CTRL_C'
+type ExpectedInput = DirectionalInput | CommandInput
 
 function main(
     render: (screen: TK.ScreenBuffer, state: GameState) => void,
@@ -35,23 +52,35 @@ function main(
 ) {
     TK.getDetectedTerminal((error, terminal) => {
         if (error) throw new Error('Cannot detect terminal.')
+        const WIDTH = terminal.width
+        const HEIGHT = terminal.height - 1
 
-        let updates = initialise
+        let updates = {
+            ...initialise,
+            game: {
+                ...initialise.game,
+                max: {
+                    x: WIDTH,
+                    y: HEIGHT,
+                },
+            },
+        }
 
         terminal.hideCursor()
         terminal.grabInput(true)
 
-        terminal.on('key', (name: string) => {
+        terminal.on('key', (name: ExpectedInput) => {
             if (name === 'CTRL_C') {
                 exit(terminal)
+                return
             }
             updates = handleControls(name, updates)
         })
 
         const screen = new TK.ScreenBuffer({
             dst: terminal,
-            width: terminal.width,
-            height: terminal.height - 1,
+            width: WIDTH,
+            height: HEIGHT,
         })
 
         render(screen, updates)
@@ -69,26 +98,47 @@ function render(screen: ScreenBuffer, state: GameState) {
             dy: 0,
             dx: 0,
             wrap: true,
-            attr: { color: 'green', bgColor: 'black' },
+            attr: { color: 'green', bgColor: 'green' },
         },
-        '!',
+        ' ',
     )
     screen.draw({ delta: true })
     setTimeout(() => render(screen, state), 50)
 }
 
-function handleControls(input: string, state: GameState): GameState {
-    if (DIRECTIONS.UP.includes(input)) {
-        state.snake.position.y -= 1
+/**
+ * TODO
+ *  - split up code
+ *  - more sophisticated snake
+ *    - constant moove
+ *  - add food
+ *  - make the long boi
+ *  - handle snek collision
+ *  - title screen
+ *  - score
+ *  - high score in external txt doc
+ */
+
+function handleControls(input: DirectionalInput, state: GameState): GameState {
+    if (DIRECTIONS.DOWN.some((dir) => dir === input)) {
+        const next = state.snake.position.y + 1
+        const checked = next >= state.game.max.y ? 0 : next
+        state.snake.position.y = checked
     }
-    if (DIRECTIONS.DOWN.includes(input)) {
-        state.snake.position.y += 1
+    if (DIRECTIONS.UP.some((dir) => dir === input)) {
+        const next = state.snake.position.y - 1
+        const checked = next < 0 ? state.game.max.y - 1 : next
+        state.snake.position.y = checked
     }
-    if (DIRECTIONS.RIGHT.includes(input)) {
-        state.snake.position.x += 1
+    if (DIRECTIONS.RIGHT.some((dir) => dir === input)) {
+        const next = state.snake.position.x + 1
+        const checked = next >= state.game.max.x ? 0 : next
+        state.snake.position.x = checked
     }
-    if (DIRECTIONS.LEFT.includes(input)) {
-        state.snake.position.x -= 1
+    if (DIRECTIONS.LEFT.some((dir) => dir === input)) {
+        const next = state.snake.position.x - 1
+        const checked = next < 0 ? state.game.max.x - 1 : next
+        state.snake.position.x = checked
     }
     return state
 }
